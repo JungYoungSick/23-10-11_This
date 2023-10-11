5d01f504638ed3007b11c05cd9cdbb11916abc05
jungYoungSick <junfas1216@gmail.comgit config --global user.email junfas1216@gmail.com>
2023-10-11 10:56:20 +0900

    This 생성자 함수

--- newfile.js
+++ newfile.js
@@ -2,6 +2,12 @@ function student(name, number) {
   this.name = name;
   this.number = number;
 }
+let list = ["이은정", "윤준현"];
+let studentArr = [];
+let test = new student(list[0], "0")
 
-let test = student("공욱재", "0")
-console.log(test)
+for(let i=0; i<list.length; i++) {
+  studentArr[i] = new student(list[0], i+1)
+}
+console.log(studentArr);
+
