8aa4c2dd1768a70053cda0b5e345cda0e348a103
jungYoungSick <junfas1216@gmail.comgit config --global user.email junfas1216@gmail.com>
2023-10-11 12:01:32 +0900

    화살표 함수

--- newArrow.js
+++ newArrow.js
@@ -1,9 +1,10 @@
 // 일종의 콜백함수
 function outerOne(a) {
-
-  function innerOne(b) {
-    console.log(b);
-  }
+  const innerOne = b => console.log(b);
   innerOne(a);
 }
 console.log(outerOne("hello"));
+
+fs.readFile("./html", data, (error) => {
+
+})
